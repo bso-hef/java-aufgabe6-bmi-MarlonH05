8787de2ece7a80972c049e8db20bdf284e731e08
Sebastian Schmitt <se.schmitt@bso-hef.de>
2025-03-28 16:46:40 +0100

    Update and rename BMI.java to Main.java

--- src/main/java/de/bsohef/Main.java
+++ src/main/java/de/bsohef/Main.java
@@ -2,7 +2,7 @@ package de.bsohef;
 
 import java.util.Scanner;
 
-public class BMI {
+public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
